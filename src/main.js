6e499e7baf7ef3dd424e044b0fd07e7aaaa932f4
李苍龙 <176152949@qq.com>
2022-10-14 10:13:55 +0800

    已修改        src/main.js

--- src/main.js
+++ src/main.js
@@ -1,33 +1,47 @@
 function LoadConfig(){
-	alert("显示卡牌：1");
-	StandardTaxRate();
+	for(var i=1;i<=3;++i) {
+		LoadCardXml(i)
+	}
+	var url = window.location.href;
+	alert("显示1：" + url);
+	var url = window.location.pathname;
+	alert("显示2：" + url);
 }
 
-function StandardTaxRate()
-{
-	$.ajax({
-	url: "/demo.xml",
-	dataType: 'xml',
-	type: 'GET',
-	timeout: 2000,
-	error: function(xml)
-	{
-		alert("加载XML 文件出错！");
-	},
-	success: function(xml)
-	{
-		alert("显示卡牌：2")	
-		$(xml).find("taxrate").each(function(i)
-		{
-		var oid = $(this).attr("id");
-		var lower = $(this).children("lower").text();
-		var upper = $(this).children("upper").text();
-		var rate = $(this).children("rate").text();
-		var buckle = $(this).children("buckle").text();
-		alert("显示卡牌："+lower)	
-	  });
+function LoadCardXml(index){
+	var xmlFile = "https://dkttlong.github.io/1/cards/"+index+".xml"
+	var xmldoc=loadXML(xmlFile);
+
+	//获得根节点
+	var cards=xmlDoc.getElementsByTagName("Card");
+	for(var i=0;i<cards.length;i++) {
+		var ID=cards[i].getAttribute("ID");
+		var SetNo=cards[i].getAttribute("SetNo");
+		var Name=cards[i].getAttribute("Name");
+		var Color=cards[i].getAttribute("Color");
+		var Type=cards[i].getAttribute("Type");
+		var Level=cards[i].getAttribute("Level");
+		var Power=cards[i].getAttribute("Power");
+		var Life=cards[i].getAttribute("Life");
+		var Rarity=cards[i].getAttribute("Rarity");
+		var RulesText=cards[i].getAttribute("RulesText");
+		var UpLvlCardId=cards[i].getAttribute("UpLvlCardId");
+		var NpcAIFlag=cards[i].getAttribute("NpcAIFlag");
 	}
-	});
+	// alert("显示卡牌：" + cards.length);
+}
+
+function loadXML(xmlFile){
+	if (window.XMLHttpRequest){
+		// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else{// code for IE6, IE5
+	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.open("GET",xmlFile,false);
+	xmlhttp.send();
+	xmlDoc=xmlhttp.responseXML;
 }
 
 function ShowCards(){
